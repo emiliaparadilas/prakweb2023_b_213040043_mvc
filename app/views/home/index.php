38be15866a78366b9db87c371591a057e469aea5
emiliaparadilas <emiliapsupriatna@gmail.com>
2023-10-21 21:34:38 +0700

    Video 6 Assets

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,4 +1,8 @@
-
-        <h1>Selamat Datang di Website Saya</h1>
-    </body>
-</html>
+<div class="container">
+    <div class="mt-4 p-5">
+        <h1 class="mt-2">Selamat Datang di Website Saya!</h1>
+        <hr class="my-4"></hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, fugit nisi. Suscipit quas possimus aspernatur.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
+    </div>
+</div> 
